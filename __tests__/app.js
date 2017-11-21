60089289870c8c19bb8dea3b6cd8072440d345ed
Ron Apelbaum <ronapelbaum@gmail.com>
2017-11-21 17:09:13 +0200

    fix artifactory

--- __tests__/app.js
+++ __tests__/app.js
@@ -5,13 +5,13 @@ const helpers = require('yeoman-test');
 
 describe('generator-aol-on:app', () => {
   beforeAll(() => {
-    return helpers.run(path.join(__dirname, '../generators/app'))
-      .withPrompts({someAnswer: true});
+    return helpers
+      .run(path.join(__dirname, '../generators/app'))
+      .withPrompts({ main: 'MainClass' });
   });
 
   it('creates files', () => {
-    assert.file([
-      'dummyfile.txt'
-    ]);
+    assert.file(['package.json']);
+    assert.file(['src/main-class.js']);
   });
 });
--- generators/app/index.js
+++ generators/app/index.js
@@ -3,92 +3,88 @@ const Generator = require('yeoman-generator');
 const chalk = require('chalk');
 const yosay = require('yosay');
 
-const toDash = str => str
-.replace(/^([A-Z])/g, $1 => $1.toLowerCase())
-.replace(/(\s)/g, $1 => "-")
-.replace(/([A-Z])/g, $1 => "-"+$1.toLowerCase());
+const toDash = str =>
+  str
+    .replace(/^([A-Z])/g, $1 => $1.toLowerCase())
+    .replace(/(\s)/g, () => '-')
+    .replace(/([A-Z])/g, $1 => '-' + $1.toLowerCase());
 
 module.exports = class extends Generator {
   prompting() {
     // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the fabulous ' + chalk.red('generator-aol-on') + ' generator!'
-      ));
+    this.log(
+      yosay('Welcome to the fabulous ' + chalk.red('generator-aol-on') + ' generator!')
+    );
 
     const prompts = [
-    {
-      type: 'input',
-      name: 'name',
-      message: 'Your project name',
-    //Defaults to the project's folder name if the input is skipped
-    default: this.appname
-  },
-  {
-    type: 'input',
-    name: 'main',
-    message: 'Main class name',
-    //Defaults to the project's folder name if the input is skipped
-    default: 'MainClass'
-  }];
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Your project name',
+        // Defaults to the project's folder name if the input is skipped
+        default: this.appname
+      },
+      {
+        type: 'input',
+        name: 'main',
+        message: 'Main class name',
+        // Defaults to the project's folder name if the input is skipped
+        default: 'MainClass'
+      }
+    ];
 
-  return this.prompt(prompts).then(props => {
+    return this.prompt(prompts).then(props => {
       // To access props later use this.props.someAnswer;
       this.props = props;
     });
-}
+  }
 
-writing() {
-  let main = this.props.main,
-  mainFile = toDash(main);
-  this.fs.copyTpl(
-    this.templatePath('script.js'),
-    this.destinationPath(`src/${mainFile}.js`),
-    { main: main }
+  writing() {
+    let main = this.props.main;
+    let mainFile = toDash(main);
+    this.fs.copyTpl(
+      this.templatePath('script.js'),
+      this.destinationPath(`src/${mainFile}.js`),
+      { main: main }
     );
-  //this.fs.copyTpl(
-    // this.templatePath('.npmrc'),
-    // this.destinationPath('.npmrc')
-    // );
-  this.fs.copyTpl(
-    this.templatePath('package.json'),
-    this.destinationPath('package.json'),
-    { appname: toDash(this.props.name),
-      mainFile: mainFile }
-      );
-}
+    this.fs.copyTpl(
+      this.templatePath('package.json'),
+      this.destinationPath('package.json'),
+      {
+        appname: toDash(this.props.name),
+        mainFile: mainFile
+      }
+    );
+  }
 
-install() {
-  this.installDependencies({
+  install() {
+    this.installDependencies({
       npm: true,
       bower: false,
       yarn: false
     });
 
-  this.npmInstall([
-  //   'aol-on-infra-utils',
-    'eslint',
-    'eslint-config-o',
-    'istanbul',
-    'jasmine-core',
-    'karma',
-    'karma-chrome-launcher',
-    'karma-cli',
-    'karma-closure',
-    'karma-coverage',
-    'karma-generic-preprocessor',
-    'karma-htmlfile-reporter',
-    'karma-jasmine',
-    'karma-junit-reporter',
-    'karma-phantomjs-launcher',
-    'phantomjs-prebuilt',
-    ], { 
-      'save-dev': true,
-      'registry': 'https://artifactory-prod.vidible.aolcloud.net/artifactory/api/npm/libs-npm',
-      'email': 'npm-push@teamaol.com'
-    });
-
-
-  
-
-}
+    this.npmInstall(
+      [
+        'eslint',
+        'istanbul',
+        'jasmine-core',
+        'karma',
+        'karma-chrome-launcher',
+        'karma-cli',
+        'karma-closure',
+        'karma-coverage',
+        'karma-generic-preprocessor',
+        'karma-htmlfile-reporter',
+        'karma-jasmine',
+        'karma-junit-reporter',
+        'karma-phantomjs-launcher',
+        'phantomjs-prebuilt'
+      ],
+      {
+        'save-dev': true,
+        'save-exact': true
+      }
+    );
+  }
 };
